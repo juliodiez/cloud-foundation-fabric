60bb3d64b4d06f16f139f01cca54885c5e0f77c1
Julio Diez <jdiez@google.com>
2022-03-18 21:46:43 +0100

    Update google provider to version 4.14.0

--- examples/networking/ncc-ra/versions.tf
+++ examples/networking/ncc-ra/versions.tf
@@ -17,7 +17,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.0.0"
+      version = ">= 4.14.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
